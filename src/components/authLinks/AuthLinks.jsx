aee69bb5f8b8ee61eba3b4d9e7765f7e02bf72f6
nivas <nivas.bn@emphaticsense.com>
2024-02-06 14:52:24 +0530

    theme toggle button

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,8 +1,22 @@
 import React from "react"
 import styles from "./authLinks.module.css"
+import Link from "next/link"
 
 const AuthLinks = () => {
-  return <div className={styles.container}>AuthLinks</div>
+  //temporory
+  const status = "notauthenciated"
+  return (
+    <>
+      {status === "notauthenciated" ? (
+        <Link href={"/login"}>Login</Link>
+      ) : (
+        <>
+          <Link href={"/write"}>Write</Link>
+          <span className={styles.link}>Logout</span>
+        </>
+      )}
+    </>
+  )
 }
 
 export default AuthLinks
--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,8 +1,15 @@
 import React from "react"
 import styles from "./themeToggle.module.css"
+import Image from "next/image"
 
 const ThemeToggle = () => {
-  return <div className={styles.container}>ThemeToggle</div>
+  return (
+    <div className={styles.container}>
+      <Image src={"/moon.png"} width={14} height={14} />
+      <div className={styles.ball}></div>
+      <Image src={"/sun.png"} width={14} height={14} />
+    </div>
+  )
 }
 
 export default ThemeToggle
