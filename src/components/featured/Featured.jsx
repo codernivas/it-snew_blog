56542fdffe213ca22b0962b04ae11dfa84d849b2
nivas <nivas.bn@emphaticsense.com>
2024-02-06 16:58:36 +0530

    featured layout

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,12 +1,33 @@
 import React from "react"
 import styles from "./featured.module.css"
+import Image from "next/image"
 
 const Featured = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>
-      
+        <b>Hey, it&#39;s new here!</b> Discover my storiesand creative ideas.
       </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src={"/p1.jpeg"} alt="" fill className={styles.image} />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>
+            Lorem ipsum is placeholder text commonly used in the graphic, print.
+          </h1>
+          <p className={styles.postDesc}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
     </div>
   )
 }
