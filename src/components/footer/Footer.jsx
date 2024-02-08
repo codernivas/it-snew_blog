277ca5c249801ed01b7765ad18c9ca6621da3afd
nivas <nivas.bn@emphaticsense.com>
2024-02-08 14:34:50 +0530

    footer page layout

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,52 @@
 import React from "react"
 import styles from "./footer.module.css"
+import Image from "next/image"
+import Link from "next/link"
 
 const Footer = () => {
-  return <div className={styles.container}>Footer</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <div className={styles.logo}>
+          <Image src={"/logo.png"} alt="logo" width={50} height={50} />
+          <h1 className={styles.logoText}>it&#39;snew</h1>
+        </div>
+        <p className={styles.desc}>
+          The passage experienced a surge in popularity during the 1960s when
+          Letraset used it on their dry-transfer sheets, and again during the
+          90s as desktop publishers bundled the text with their software. Today
+          its seen all around the web
+        </p>
+        <div className={styles.icons}>
+          <Image src={"/facebook.png"} alt="logo" width={18} height={18} />
+          <Image src={"/instagram.png"} alt="logo" width={18} height={18} />
+          <Image src={"/youtube.png"} alt="logo" width={18} height={18} />
+        </div>
+      </div>
+      <div className={styles.links}>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Links</span>
+          <Link href={"/"}>HomePage</Link>
+          <Link href={"/"}>Blog</Link>
+          <Link href={"/"}>About</Link>
+          <Link href={"/"}>Contact</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Tags</span>
+          <Link href={"/"}>Style</Link>
+          <Link href={"/"}>Fashion</Link>
+          <Link href={"/"}>Coding</Link>
+          <Link href={"/"}>Travel</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Social</span>
+          <Link href={"/"}>Facebook</Link>
+          <Link href={"/"}>Instagram</Link>
+          <Link href={"/"}>Youtube</Link>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Footer
