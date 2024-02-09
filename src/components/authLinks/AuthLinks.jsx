f3a42474c6ac7558f152322291c2f8b6fb235239
nivas <nivas.bn@emphaticsense.com>
2024-02-09 11:01:08 +0530

    prisma mongodb integrated

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -2,15 +2,15 @@
 import React, { useState } from "react"
 import styles from "./authLinks.module.css"
 import Link from "next/link"
-import { signOut } from "next-auth/react"
+import { signOut, useSession } from "next-auth/react"
 
 const AuthLinks = () => {
   const [open, setOpen] = useState(false)
-  //temporory
-  const status = "authenciated"
+
+  const {status} = useSession()
   return (
     <>
-      {status === "notauthenciated" ? (
+      {status === "unauthenticated" ? (
         <Link href={"/login"} className={styles.link}>
           Login
         </Link>
@@ -19,7 +19,9 @@ const AuthLinks = () => {
           <Link href={"/write"} className={styles.link}>
             Write
           </Link>
-          <span className={styles.link} onClick={signOut}>Logout</span>
+          <span className={styles.link} onClick={signOut}>
+            Logout
+          </span>
         </>
       )}
       <div className={styles.burger} onClick={() => setOpen(!open)}>
@@ -37,7 +39,7 @@ const AuthLinks = () => {
           ) : (
             <>
               <Link href={"/write"}>Write</Link>
-              <span className={styles.link} >Logout</span>
+              <span className={styles.link}>Logout</span>
             </>
           )}
         </div>
